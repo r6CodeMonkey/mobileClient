24173f73b51f2fb81b01b07315790a2f8acf3bc5
Tim <wright.tim.j@gmail.com>
2015-12-06 22:59:30 +0000

    sub routine switch statements

--- mobile/src/main/java/oddymobstar/activity/handler/ActivityResultHandler.java
+++ mobile/src/main/java/oddymobstar/activity/handler/ActivityResultHandler.java
@@ -44,76 +44,80 @@ public class ActivityResultHandler {
             case MaterialsListener.USER_IMAGE_RESULT_CODE:
 
                 if (resultCode == main.RESULT_OK) {
-                    try {
-                        // We need to recyle unused bitmaps
-
-                        InputStream stream = main.getContentResolver().openInputStream(
-                                data.getData());
-                        Bitmap bitmap = BitmapFactory.decodeStream(stream);
-                        stream.close();
-                        bitmap = Bitmap.createScaledBitmap(bitmap, 236, 354, false);
-                        Log.d("bitmap size is", "size " + bitmap.getRowBytes() * bitmap.getHeight());
-                        controller.materialsHelper.userImageView.setImageBitmap(bitmap);
-                        byte[] imageArray;
-                        if (controller.materialsHelper.userImage == null) {
-                            controller.materialsHelper.userImage = new UserImage();
-                            controller.materialsHelper.userImage.setUserImageKey(controller.configuration.getConfig(Configuration.PLAYER_KEY).getValue());
-                            controller.materialsHelper.userImage.setUserImage(bitmap);
-                            imageArray = controller.dbHelper.addUserImage(controller.materialsHelper.userImage);
-                        } else {
-                            controller.materialsHelper.userImage.setUserImage(bitmap);
-                            imageArray = controller.dbHelper.updateUserImage(controller.materialsHelper.userImage);
-                        }
-
-                        try {
-                            final OutImageMessage outImageMessage = new OutImageMessage(controller.locationListener.getCurrentLocation(), controller.configuration.getConfig(Configuration.PLAYER_KEY).getValue(), controller.uuidGenerator.generateAcknowledgeKey());
-                            outImageMessage.setImage(Base64.encodeToString(imageArray, Base64.DEFAULT));
-
-                            new Thread((new Runnable() {
-                                @Override
-                                public void run() {
-                                    controller.cheService.writeToSocket(outImageMessage);
-                                }
-                            })).start();
-
-
-                        } catch (JSONException jse) {
-
-                        } catch (NoSuchAlgorithmException nsae) {
-
-                        }
-
-                        //need to review this too.! bitmap.recycle();
-
-
-                    } catch (FileNotFoundException fe) {
-                    } catch (IOException e) {
-
-                    }
+                    handleImage( data);
                 }
-
-
                 return;
 
             case Bluetooth.REQUEST_ENABLE_BT:
-
-
-                switch (controller.connectivityHandler.getMode()) {
-                    case ConnectivityDialog.BLUETOOTH:
-
-                        controller.bluetoothReceiver = controller.connectivityHandler.getBluetooth().getReceiver();
-                        main.registerReceiver(controller.bluetoothReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
-
-                        controller.connectivityHandler.getBluetooth().handle(requestCode, resultCode, data);
-                        if (resultCode == controller.connectivityHandler.getBluetooth().DISCOVERABLE_SECONDS) {
-                            controller.deviceDiscoveryHandler.init();
-                            controller.connectivityHandler.getBluetooth().getProgress(controller.deviceDiscoveryHandler).show();
-                        }
-                        break;
-
-                }
-
+                handleBluetooth(requestCode, resultCode, data);
                 return;
+        }
+    }
+
+    private void handleBluetooth(int requestCode, int resultCode, Intent data){
+
+        switch (controller.connectivityHandler.getMode()) {
+            case ConnectivityDialog.BLUETOOTH:
+
+                controller.bluetoothReceiver = controller.connectivityHandler.getBluetooth().getReceiver();
+                main.registerReceiver(controller.bluetoothReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+
+                controller.connectivityHandler.getBluetooth().handle(requestCode, resultCode, data);
+                if (resultCode == controller.connectivityHandler.getBluetooth().DISCOVERABLE_SECONDS) {
+                    controller.deviceDiscoveryHandler.init();
+                    controller.connectivityHandler.getBluetooth().getProgress(controller.deviceDiscoveryHandler).show();
+                }
+                break;
+
+        }
+    }
+
+    private void handleImage(Intent data){
+        try {
+            // We need to recyle unused bitmaps
+
+            InputStream stream = main.getContentResolver().openInputStream(
+                    data.getData());
+            Bitmap bitmap = BitmapFactory.decodeStream(stream);
+            stream.close();
+            bitmap = Bitmap.createScaledBitmap(bitmap, 236, 354, false);
+            Log.d("bitmap size is", "size " + bitmap.getRowBytes() * bitmap.getHeight());
+            controller.materialsHelper.userImageView.setImageBitmap(bitmap);
+            byte[] imageArray;
+            if (controller.materialsHelper.userImage == null) {
+                controller.materialsHelper.userImage = new UserImage();
+                controller.materialsHelper.userImage.setUserImageKey(controller.configuration.getConfig(Configuration.PLAYER_KEY).getValue());
+                controller.materialsHelper.userImage.setUserImage(bitmap);
+                imageArray = controller.dbHelper.addUserImage(controller.materialsHelper.userImage);
+            } else {
+                controller.materialsHelper.userImage.setUserImage(bitmap);
+                imageArray = controller.dbHelper.updateUserImage(controller.materialsHelper.userImage);
+            }
+
+            try {
+                final OutImageMessage outImageMessage = new OutImageMessage(controller.locationListener.getCurrentLocation(), controller.configuration.getConfig(Configuration.PLAYER_KEY).getValue(), controller.uuidGenerator.generateAcknowledgeKey());
+                outImageMessage.setImage(Base64.encodeToString(imageArray, Base64.DEFAULT));
+
+                new Thread((new Runnable() {
+                    @Override
+                    public void run() {
+                        controller.cheService.writeToSocket(outImageMessage);
+                    }
+                })).start();
+
+
+            } catch (JSONException jse) {
+
+            } catch (NoSuchAlgorithmException nsae) {
+
+            }
+
+            //need to review this too.! bitmap.recycle();
+
+
+        } catch (FileNotFoundException fe) {
+        } catch (IOException e) {
+
         }
     }
 
--- mobile/src/main/java/oddymobstar/activity/handler/OnOptionsItemSelectedHandler.java
+++ mobile/src/main/java/oddymobstar/activity/handler/OnOptionsItemSelectedHandler.java
@@ -57,119 +57,121 @@ public class OnOptionsItemSelectedHandler {
                 return true;
 
             case R.id.settings:
-
-                controller.materialsHelper.navDrawer.closeDrawer(controller.materialsHelper.navigationView);
-                controller.materialsHelper.navToolbar.setTitle(R.string.menu_settings);
-
-                controller.fragmentHandler.confFrag.init(controller.configurationHandler, controller.dbHelper.getConfigs(Config.USER), controller.dbHelper.getConfigs(Config.SYSTEM));
-
-
-                transaction.replace(R.id.chat_fragment, controller.fragmentHandler.confFrag);
-                transaction.addToBackStack(null);
-                transaction.commit();
-
+                handleSettings(transaction);
                 break;
-
 
             case R.id.alliances:
-
-
-                controller.materialsHelper.navDrawer.closeDrawer(controller.materialsHelper.navigationView);
-
-                controller.materialsHandler.handleFABChange(MaterialsHelper.ALLIANCE_COLOR, R.drawable.ic_add_circle_white_24dp, View.VISIBLE);
-
-                controller.materialsListener.FAB_MODE = MaterialsListener.ALLIANCE_FAB;
-                controller.materialsHandler.handleNavToolbar(main.getResources().getColor(android.R.color.holo_red_dark), main.getResources().getString(R.string.menu_alliances));
-
-                controller.fragmentHandler.gridFrag.init(GridFragment.MY_ALLIANCES, controller.viewListener.getListClickListener());
-                transaction.replace(R.id.chat_fragment, controller.fragmentHandler.gridFrag);
-                transaction.addToBackStack(null);
-                transaction.commit();
-
+                handleAlliance(transaction);
                 break;
-
             case R.id.bluetooth_receive:
                 //same mechanism for discovery.
                 controller.viewHandler.allianceInvite(true);
-
                 break;
             case R.id.bluetooth_send:
                 //same mechanism for discovery.
                 controller.viewHandler.allianceInvite(false);
-
                 break;
-
             case R.id.utm:
-                controller.mapHandler.CURRENT_GRID_FAB_STATE = MapHandler.UTM_FAB_STATE;
-                if (!controller.mapHandler.lastLocateUTMs.isEmpty()) {
-
-                    for (final Polygon polygon : controller.mapHandler.lastLocateUTMs.values()) {
-                        main.runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-
-                                polygon.remove();
-                            }
-                        });
-                    }
-                    controller.mapHandler.CLEAR_GRIDS = true;
-
-
-                }
-
-                controller.materialsHandler.handleNavToolbar(main.getResources().getColor(android.R.color.holo_purple), main.getResources().getString(R.string.menu_utm));
-                controller.materialsHandler.handleFABChange(MaterialsHelper.UTM_COLOR, -1, View.VISIBLE);
-
-                MaterialsListener.FAB_MODE = MaterialsListener.GRID_FAB;
-                controller.mapHandler.setSelectedGrid();
-
-                controller.mapHandler.animateToGrid(controller.mapHelper.getMyUTM(), MapHandler.UTM_ZOOM);
-                controller.materialsHelper.navDrawer.closeDrawer(controller.materialsHelper.navigationView);
-
-                try {
-                    transaction.add(R.id.grid_view_fragment, controller.fragmentHandler.gridViewFragment);
-                } catch (Exception e) {
-
-                }
-
-                transaction.commit();
+                handleUTM(transaction);
                 break;
-
             case R.id.sub_utm:
-                controller.mapHandler.CURRENT_GRID_FAB_STATE = MapHandler.SUBUTM_FAB_STATE;
-                if (controller.mapHandler.lastLocateSubUTM != null) {
-                    controller.mapHandler.lastLocateSubUTM.remove();
-                }
-                controller.mapHandler.lastLocateSubUTM = null;
-                controller.materialsHandler.handleNavToolbar(main.getResources().getColor(android.R.color.holo_orange_dark), main.getResources().getString(R.string.menu_subutm));
-                controller.materialsHandler.handleFABChange(MaterialsHelper.SUB_UTM_COLOR, -1, View.VISIBLE);
-
-                MaterialsListener.FAB_MODE = MaterialsListener.GRID_FAB;
-
-                controller.mapHandler.setSelectedGrid();
-
-                controller.materialsHelper.navDrawer.closeDrawer(controller.materialsHelper.navigationView);
-                controller.mapHandler.animateToGrid(controller.mapHelper.getMySubUTM(), MapHandler.SUB_UTM_ZOOM);
-
-                try {
-                    transaction.replace(R.id.grid_view_fragment, controller.fragmentHandler.gridViewFragment);
-                } catch (Exception e) {
-
-                }
-
-                transaction.commit();
-
+                handleSubUTM(transaction);
                 break;
-
             case R.id.encrypt:
                 break;
-
             case R.id.invite:
                 break;
 
         }
 
         return true;
+    }
+
+    private void handleUTM(android.support.v4.app.FragmentTransaction transaction){
+        controller.mapHandler.CURRENT_GRID_FAB_STATE = MapHandler.UTM_FAB_STATE;
+        if (!controller.mapHandler.lastLocateUTMs.isEmpty()) {
+
+            for (final Polygon polygon : controller.mapHandler.lastLocateUTMs.values()) {
+                main.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        polygon.remove();
+                    }
+                });
+            }
+            controller.mapHandler.CLEAR_GRIDS = true;
+        }
+
+        controller.materialsHandler.handleNavToolbar(main.getResources().getColor(android.R.color.holo_purple), main.getResources().getString(R.string.menu_utm));
+        controller.materialsHandler.handleFABChange(MaterialsHelper.UTM_COLOR, -1, View.VISIBLE);
+
+        MaterialsListener.FAB_MODE = MaterialsListener.GRID_FAB;
+        controller.mapHandler.setSelectedGrid();
+
+        controller.mapHandler.animateToGrid(controller.mapHelper.getMyUTM(), MapHandler.UTM_ZOOM);
+        controller.materialsHelper.navDrawer.closeDrawer(controller.materialsHelper.navigationView);
+
+        try {
+            transaction.add(R.id.grid_view_fragment, controller.fragmentHandler.gridViewFragment);
+        } catch (Exception e) {
+
+        }
+
+        transaction.commit();
+    }
+
+    private void handleSubUTM(android.support.v4.app.FragmentTransaction transaction){
+        controller.mapHandler.CURRENT_GRID_FAB_STATE = MapHandler.SUBUTM_FAB_STATE;
+        if (controller.mapHandler.lastLocateSubUTM != null) {
+            controller.mapHandler.lastLocateSubUTM.remove();
+        }
+        controller.mapHandler.lastLocateSubUTM = null;
+        controller.materialsHandler.handleNavToolbar(main.getResources().getColor(android.R.color.holo_orange_dark), main.getResources().getString(R.string.menu_subutm));
+        controller.materialsHandler.handleFABChange(MaterialsHelper.SUB_UTM_COLOR, -1, View.VISIBLE);
+
+        MaterialsListener.FAB_MODE = MaterialsListener.GRID_FAB;
+
+        controller.mapHandler.setSelectedGrid();
+
+        controller.materialsHelper.navDrawer.closeDrawer(controller.materialsHelper.navigationView);
+        controller.mapHandler.animateToGrid(controller.mapHelper.getMySubUTM(), MapHandler.SUB_UTM_ZOOM);
+
+        try {
+            transaction.replace(R.id.grid_view_fragment, controller.fragmentHandler.gridViewFragment);
+        } catch (Exception e) {
+
+        }
+
+        transaction.commit();
+
+    }
+
+    private void handleAlliance(android.support.v4.app.FragmentTransaction transaction){
+        controller.materialsHelper.navDrawer.closeDrawer(controller.materialsHelper.navigationView);
+
+        controller.materialsHandler.handleFABChange(MaterialsHelper.ALLIANCE_COLOR, R.drawable.ic_add_circle_white_24dp, View.VISIBLE);
+
+        controller.materialsListener.FAB_MODE = MaterialsListener.ALLIANCE_FAB;
+        controller.materialsHandler.handleNavToolbar(main.getResources().getColor(android.R.color.holo_red_dark), main.getResources().getString(R.string.menu_alliances));
+
+        controller.fragmentHandler.gridFrag.init(GridFragment.MY_ALLIANCES, controller.viewListener.getListClickListener());
+        transaction.replace(R.id.chat_fragment, controller.fragmentHandler.gridFrag);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+    }
+
+    private void handleSettings(android.support.v4.app.FragmentTransaction transaction){
+
+        controller.materialsHelper.navDrawer.closeDrawer(controller.materialsHelper.navigationView);
+        controller.materialsHelper.navToolbar.setTitle(R.string.menu_settings);
+
+        controller.fragmentHandler.confFrag.init(controller.configurationHandler, controller.dbHelper.getConfigs(Config.USER), controller.dbHelper.getConfigs(Config.SYSTEM));
+
+        transaction.replace(R.id.chat_fragment, controller.fragmentHandler.confFrag);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 
 }
